501371e0a091140632eb8eb52eb23d3d39e977b1
TofinDev <39640205+TofinDev@users.noreply.github.com>
2018-05-26 15:34:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,13 +35,13 @@
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Location"
-    else if($message == "พิกัดสยามพารากอน"){
+    else if($message == "Home"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "location";
-        $arrayPostData['messages'][0]['title'] = "สยามพารากอน";
-        $arrayPostData['messages'][0]['address'] =   "13.7465354,100.532752";
-        $arrayPostData['messages'][0]['latitude'] = "13.7465354";
-        $arrayPostData['messages'][0]['longitude'] = "100.532752";
+        $arrayPostData['messages'][0]['title'] = "Go to my home";
+        $arrayPostData['messages'][0]['address'] =   "13.8630683,100.4829692";
+        $arrayPostData['messages'][0]['latitude'] = "13.8630683";
+        $arrayPostData['messages'][0]['longitude'] = "100.4829692";
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Text + Sticker ใน 1 ครั้ง"

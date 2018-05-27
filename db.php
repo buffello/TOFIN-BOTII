07fed985aa7abb9424b2ed69db2addaa9feb7e79
TofinDev <39640205+TofinDev@users.noreply.github.com>
2018-05-27 16:24:02 +0700

    Update db.php

--- db.php
+++ db.php
@@ -14,8 +14,8 @@ $_msg = $arrJson['events'][0]['message']['text'];
  
  
 $api_key="<MLAB APIKEY>";
-$url = 'https://api.mlab.com/api/1/databases/tofin/collections/linebot?apiKey='.$api_key.'';
-$json = file_get_contents('https://api.mlab.com/api/1/databases/tofin/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
+$url = 'https://api.mlab.com/api/databases/tofin/collections/linebot?apiKey='.$api_key.'';
+$json = file_get_contents('https://api.mlab.com/api/databases/tofin/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
 $data = json_decode($json);
 $isData=sizeof($data);
 
